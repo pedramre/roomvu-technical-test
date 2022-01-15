1c69e18d9aad14c2af42c22b1c31a60bdc1d4e48
Pedram <pdmr.1992@gmail.om>
2022-01-15 19:14:11 +0330

    Add them toggle button

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,26 @@
+import {useEffect, useState} from "react";
+import Toggle from 'react-toggle'
+import "react-toggle/style.css"
+import {DayToggle} from "./DayToggle";
+import {NightToggle} from "./NightToggle";
+
 export const Header = ()=>{
+    const[theme,setTheme] = useState(false);
+
     return (
         <header className="">
             <div className="header w-100">
                 <h1>
                     <a href="">Overreacted</a>
                 </h1>
-                <img width="6%" src="/images/switch.png" alt="" />
+                <Toggle
+                    id='status'
+                    icons={{
+                        unchecked: <DayToggle/>,
+                        checked: <NightToggle/>,
+                    }}
+                    defaultChecked={theme}/>
+
             </div>
         </header>
     )
